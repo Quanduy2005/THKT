36d672d656c5fb6f832a7645979e7216d4b4dba7
Nguyễn Duy Quân <nguyenduyquan2023ht@gmail.com>
2024-12-13 09:48:02 +0700

    Add files via upload

---
+++ Bai8/8.3.py
@@ -0,0 +1,17 @@
+﻿print("Sinh vien: Nguyen Duy Quan")
+print("Ma so SV:235752021610107")
+print("**************************")
+
+import turtle
+colors = ["red", "green", "blue"]
+painter = turtle.Turtle()
+painter.pensize(3)
+for i in range(12):
+    color = colors[i % len(colors)] 
+    painter.pencolor(color)
+    painter.circle(100)
+    painter.right(30)
+    painter.left(60)
+    painter.setposition(0, 0)
+painter.hideturtle()
+turtle.done()
